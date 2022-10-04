b3d9b9be5f97ee4b53d848bc0fa56042ad769c53
MuhammaddiyorAbdullayev <muhammddiyorabdullayev@gmail.com>
2022-10-04 21:07:06 +0500

    xullas push qildim

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { BigsFooter, Email,Phone1, Footer1, Phone, PhoneFooter, SmallFooter, BigLetter, Icons } from './styleF'
+import { BigsFooter, Email,Phone1, Footer1, Phone, PhoneFooter, SmallFooter, BigLetter, Icons, Input, Send, Copy } from './styleF'
 import EmailIcon from '../../Assets/footer/email.png'
 import PhoneIcon from '../../Assets/footer/Call.png'
-import { Facebook,Twitter,YouTube,Instagram } from '@mui/icons-material'
+import Vectr from '../../Assets/footer/Vector.png'
+import {Facebook, Instagram, Twitter, YouTube,} from '@mui/icons-material'
+import SendSharpIcon from '@mui/icons-material/SendSharp';
 const Footer = () => {
   return (
+    
     <>
     <BigsFooter>
         <PhoneFooter>
@@ -24,24 +27,43 @@ const Footer = () => {
         <SmallFooter>
             <Footer1>
                 <BigLetter>
-                <h2>Tammy <span>Food</span></h2>
+                    <h2>Tammy <span>Food</span></h2>
                 </BigLetter>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                 <Icons>
-                    <img src={Facebook} alt="" />
-                    <img src={Instagram} alt="" />
-                    <img src={Twitter} alt="" />
-                    <img src={YouTube} alt="" />
+                    <Facebook/>
+                    <Instagram/>
+                    <Twitter/>
+                    <YouTube/>
                 </Icons>
             </Footer1>
             <Footer1>
-    
+                <BigLetter>
+                    <h2>QUICK LINK</h2>
+                </BigLetter>
+                <h3>About Us</h3>
+                <h3>Menu</h3>
+                <h3>Blog</h3>
+                <h3>Contact Us</h3>
             </Footer1>
             <Footer1>
-
+                <BigLetter>
+                    <h2>News Letter</h2>
+                </BigLetter>
+                <p>Subscribe our newsletter to get our latest
+                update & news</p>
+                <Input>
+                    <input type="text" placeholder='Your email address' />
+                    <Send>
+                        <SendSharpIcon/>
+                    </Send>
+                </Input>
             </Footer1>
         </SmallFooter>
+        <Copy>
+            <p> <img src={Vectr} alt="" /> Copyright 2021 .Tammy Food. All Right Reserved.</p>
+        </Copy>
     </BigsFooter>
     </>
   )
--- src/Components/Footer/styleF.jsx
+++ src/Components/Footer/styleF.jsx
@@ -3,18 +3,18 @@ import styled from "styled-components";
 export const BigsFooter = styled.div`
     width: 100%;
     height: auto;
-    border: 3px solid red;
+    /* border: 3px solid red; */
     background-color: #232233;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: center;
     flex-direction: column;
     margin-top: 120px;
 `
 export const PhoneFooter = styled.div`
     width: 80%;
-    height: 230px;
-    border: 2px solid blue;
+    height: 200px;
+    /* border: 2px solid blue; */
     margin-top: -100px;
     margin-bottom: 30px;
     border-radius: 30px;
@@ -29,7 +29,7 @@ export const PhoneFooter = styled.div`
 export const Email = styled.div`
     width: 50%;
     height: auto;
-    border: 2px solid #E6E6E6;
+    /* border: 2px solid #E6E6E6; */
     display: flex;
     align-items: center;
     justify-content: center;
@@ -38,6 +38,16 @@ export const Email = styled.div`
     img{
         width: 80px;
         height: 80px;
+        @media screen and (max-width:1284px){
+        height: 80px;
+        width: 80px;
+        font-size: 10px;
+    }
+    }
+    @media screen and (max-width:1284px){
+        height: 80px;
+        width: 45%;
+        font-size: 10px;
     }
 `
 export const Phone = styled.div`
@@ -59,15 +69,15 @@ export const Phone1 = styled.div`
 export const SmallFooter = styled.div`
     width: 80%;
     height: auto;
-    border: 3px solid white;
+    /* border: 3px solid white; */
     display: flex;
     align-items: center;
     justify-content: space-between;
 `
 export const Footer1 = styled.div`
-    width: 33%;
+    width: 32%;
     height: 230px;
-    border: 3px solid yellow;
+    /* border: 3px solid yellow; */
     display: flex;
     align-items: flex-start;
     flex-direction: column;
@@ -78,7 +88,7 @@ export const Footer1 = styled.div`
 export const BigLetter = styled.div`
     width: 100%;
     height: auto;
-    border: 1px solid white;
+    /* border: 1px solid white; */
     display: flex;
     align-items: center;
     justify-content: flex-start;
@@ -87,8 +97,49 @@ export const BigLetter = styled.div`
     }
 `
 export const Icons = styled.div`
-    width: 100%;
+    width: 50%;
     height: 30px;
-    border: 1px solid white;
+    /* border: 1px solid white; */
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    img{
+        color: white;
+    }
+`
+export const Input = styled.div`
+    width: 100%;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    background-color:#FEB918;
+    border: none;
+    border-radius: 4px;
+    input{
+        width: 85%;
+        height: 40px;
+        border: none;
+        border-radius: 4px;
+    }
+
+`
+export const Send = styled.div`
+    color:#fff;
+    width: 30px;
+    font-size: 30px;
+    margin-left: 20px;
+    border-radius: 4px;
+`
+export const Copy = styled.div`
+    width: 80%;
+    height: 50px;
+    /* border: 1px solid #fff; */
+    border-bottom:none ;
+    border-left: none;
+    border-right: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     color: white;
+    font-size: 20px;
 `
